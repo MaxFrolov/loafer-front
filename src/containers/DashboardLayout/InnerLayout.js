6c8339751154a8e519ce0f73a17f4196922f274b
Venumteam <maxtor2808@gmail.com>
2016-11-29 18:10:07 +0200

    FEATURE: add upcoming events in dashboard layout

--- src/containers/DashboardLayout/InnerLayout.js
+++ src/containers/DashboardLayout/InnerLayout.js
@@ -1,7 +1,13 @@
 import React, { Component, PropTypes } from 'react'
 // components
 import { InHeader } from 'components'
+import { asyncConnect } from 'redux-async-connect'
 
+@asyncConnect([
+  { key: 'participant_events',
+    promise: ({ helpers }) => helpers.client.get('events/participant_events')
+  }
+])
 export default class InnerLayout extends Component {
   static propTypes = {
     children: PropTypes.object

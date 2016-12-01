c65716dc6d6b6f3b5782a0983821a6b452f745d6
Venumteam <maxtor2808@gmail.com>
2016-12-01 09:47:18 +0200

    CREATE: Groups initial

--- src/containers/DashboardLayout/Groups/Groups.js
+++ src/containers/DashboardLayout/Groups/Groups.js
@@ -1,9 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
+// components
 import Helmet from 'react-helmet'
+import { asyncConnect } from 'redux-async-connect'
+// constants
+const avatarPlaceholder = require('../../../../static/user.svg')
 
+@asyncConnect([
+  { key: 'userGroups',
+    promise: ({ helpers }) => helpers.client.get('groups/user_groups')
+  }
+])
 export default class Groups extends Component {
+  static propTypes = {
+    userGroups: PropTypes.object.isRequired
+  };
 
   render () {
+    const { userGroups } = this.props
     return (
       <div>
         <Helmet title="Groups"/>
@@ -11,6 +24,27 @@ export default class Groups extends Component {
           <h1>
             Groups
           </h1>
+          <div className="panel panel-default">
+            <div className="panel-body">
+              {userGroups.resources.length && <div className="list-group">
+                {userGroups.resources.map((group, idx) => (
+                  <div className="list-group-item" key={idx}>
+                    <div className="clearfix">
+                      <div className="pull-left">
+                        <h5>{group.title}</h5>
+                      </div>
+                      <div className="pull-right">
+                        {group.group_participants.map((participant, index) => (
+                          <img className="img-circle" src={participant.avatar_url || avatarPlaceholder}
+                            alt="User avatar" key={index} style={{ maxWidth: '30px' }} />
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>}
+            </div>
+          </div>
         </div>
       </div>
     )
